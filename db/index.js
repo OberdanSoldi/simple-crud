c8aa25a1b4cccfddc4b142c6a932ffe30139c22b
oberdansoldi <oberdan_soldi@outlook.com.br>
2022-03-06 22:56:32 -0300

    mysql

---
+++ db/index.js
@@ -0,0 +1,25 @@
+const mysql = require('serverless-mysql');
+
+require('dotenv').config()
+
+const conn = mysql({
+  config: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      port: process.env.DB_PORT,
+      password: process.env.DB_PASS,
+      database: process.env.DB_DB,
+      charset: 'utf8mb4_unicode_ci'
+  }
+});
+
+module.exports = async function query(q, values) {
+  try {
+      const results = await conn.query(q, values);
+      await conn.end();
+      return results;
+  } catch (e) {
+      throw Error(e.message);
+      
+  }
+}
