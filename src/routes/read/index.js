9d54219eb46c5a52665d1b210f83052bf5656179
oberdansoldi <oberdan_soldi@outlook.com.br>
2022-04-08 12:02:38 -0300

    removing commented code from read route

--- src/routes/read/index.js
+++ src/routes/read/index.js
@@ -21,29 +21,3 @@ router.get('/', async (req, res) => {
 })
 
 export default router;
-
-
-// module.exports = async (req, res) => {
-// 	try {
-// 		const sql = `SELECT * FROM crud`;
-// 		const results = await query(sql);
-// 		if (results.length > 0) {
-// 			res.status(200).json({
-// 				message: 'Success',
-// 				status: '200',
-// 				data: results
-// 			});
-// 		} else {
-// 			res.status(404).json({
-// 				message: 'Not found',
-// 				status: '404'
-// 			});
-// 		}
-// 	} catch (e) {
-// 		res.status(500).json({
-// 			message: 'Internal server error',
-// 			status: '500'
-// 		});
-// 		console.error(e);
-// 	}
-// };
